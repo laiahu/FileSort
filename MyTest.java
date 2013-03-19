ea0b75da13f2885c2ce4fb5cbf315b3d6258a683
laiahu <laiahu@126.com>
2013-03-19 10:01:19 +0800

    Update MyTest.java

--- MyTest.java
+++ MyTest.java
@@ -3,5 +3,6 @@ public class MyTest{
     public void static main(String[] args){
         System.out.println("this is my test!");
         System.out.println("my hello word!");
+        System.out.println("my hello word2!");
     }
 }
